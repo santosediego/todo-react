80d67d78b991edbe2e3f81305c44715111de70f8
Diego Santos <diegosantos.dev@outlook.com>
2023-03-10 06:20:08 -0300

    Created type Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
 
 function App() {
+
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar pão na padaria', done: false },
+    { id: 2, name: 'Pegar correspodência', done: false },
+  ]);
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        {/* Área para adicionar nova tarefa */}
+
+        {/* Área para lista de tarefas */}
+        {list.map((item, index) => (
+          <div>{item.name}</div>
+        ))}
+
       </C.Area>
     </C.Container>
   );
