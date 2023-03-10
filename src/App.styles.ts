93093eefd43a2537261e2fb30b0bab478d82bf3b
Diego Santos <diegosantos.dev@outlook.com>
2023-03-10 05:56:07 -0300

    Project created and configured

---
+++ src/App.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #17181F;
+    color: #797A81;
+    min-height: 100vh;
+`;
+
+export const Area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 10px;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as C from './App.styles';
+
+function App() {
+  return (
+    <C.Container>
+      <C.Area>Hello World</C.Area>
+    </C.Container>
+  );
+}
+
+export default App;
